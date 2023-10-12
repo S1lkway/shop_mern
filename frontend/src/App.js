6a07ceb9889262ee4106c10151e570bd9732c128
Artem <silkwayway@gmail.com>
2023-10-12 16:11:13 -0400

    basic components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+//-Components
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
-    <div className="App">
-      Shop
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+
+          <Header />
+
+          <Footer />
+
+        </div>
+      </Router>
+      <ToastContainer autoClose={1500} />
+    </>
   );
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header() {
+  return (
+    <header className='header'>
+      Header
+    </header >
+  )
+}
+
+export default Header
