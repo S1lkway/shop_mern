e710571b15297bb58f5c5ece22df49d2f8073e22
Artem <silkwayway@gmail.com>
2023-10-19 11:12:42 -0400

    account list

--- frontend/src/components/userModal/Profile.jsx
+++ frontend/src/components/userModal/Profile.jsx
@@ -1,4 +1,11 @@
 import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+//-MUI icons
+import ChairOutlinedIcon from '@mui/icons-material/ChairOutlined';
+import InventoryOutlinedIcon from '@mui/icons-material/InventoryOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 // - Redux
 import { logout, reset } from '../../features/auth/authSlice'
 
@@ -12,12 +19,41 @@ function Profile() {
   }
 
   return (
-    <div><button
-      className='userModalFormButton'
-      onClick={onLogout}
-      type='submit' >
-      Sign out
-    </button></div>
+    <div className='flex_column userModalProfile'>
+      <ul className='userModalProfileList'>
+        <li>
+          <Link to='/profile'>
+            <span><ChairOutlinedIcon /> My designs & rooms</span>
+          </Link>
+        </li>
+        <li>
+          <Link to='/profile'>
+            <span><InventoryOutlinedIcon /> Purchase history</span>
+          </Link>
+        </li>
+        <li>
+          <Link to='/profile'>
+            <span><FormatListBulletedOutlinedIcon /> Shopping list</span>
+          </Link>
+        </li>
+        <li>
+          <Link to='/profile'>
+            <span><LocalShippingOutlinedIcon /> Track your order</span>
+          </Link>
+        </li>
+        <li>
+          <Link>
+            <span><AccountCircleOutlinedIcon /> Manage account</span>
+          </Link>
+        </li>
+      </ul >
+      <button
+        className='userModalFormButton'
+        onClick={onLogout}
+        type='submit' >
+        Sign out
+      </button>
+    </div>
   )
 }
 
