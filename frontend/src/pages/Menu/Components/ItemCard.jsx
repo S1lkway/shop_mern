048c1ac880e29396019d6136ef27d11ecb88d94f
Artem <silkwayway@gmail.com>
2023-10-23 14:29:03 -0400

    basic itemCard

---
+++ frontend/src/pages/Menu/Components/ItemCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ItemCard() {
+  // Category(part of menu)
+  // Name
+  // Image
+  // Description: String
+  // Type: Vegeterian, Spicy, None
+  // Popular: Yes, No
+
+  return (
+    <div>ItemCard</div>
+  )
+}
+
+export default ItemCard
