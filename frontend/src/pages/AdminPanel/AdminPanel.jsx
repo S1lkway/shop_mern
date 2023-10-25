b1426abe342efd2d5a3362232800626c4e125cf3
Artem <silkwayway@gmail.com>
2023-10-25 08:42:39 -0400

    admin panel links

--- frontend/src/pages/AdminPanel/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel/AdminPanel.jsx
@@ -1,7 +1,10 @@
 // import { useEffect } from 'react'
 // import { useSelector } from 'react-redux'
-// import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 // import { toast } from 'react-toastify'
+//-MUI icons
+import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
 
 function AdminPanel() {
   // const navigate = useNavigate()
@@ -9,7 +12,20 @@ function AdminPanel() {
 
 
   return (
-    <div>AdminPanel</div>
+    <>
+      <div className="adminPanelLinks">
+        <Link
+          to='/menu'
+          title='Add menu section'>
+          <h3><MenuBookOutlinedIcon /> Menu Section</h3>
+        </Link>
+        <Link
+          to='/deals'
+          title='Add a dish for menu'>
+          <h3><FastfoodIcon /> Dish for menu</h3>
+        </Link>
+      </div>
+    </>
   )
 }
 
