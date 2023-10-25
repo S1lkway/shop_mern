abf99eec37fb3d456ce39936539be76b65b51451
Artem <silkwayway@gmail.com>
2023-10-25 09:38:16 -0400

    default form

--- frontend/src/pages/AdminPanel/MenuSections/MenuSections.jsx
+++ frontend/src/pages/AdminPanel/MenuSections/MenuSections.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+// import { useState } from 'react'
 
 function MenuSections() {
+
+  // const [formData, setFormData] = useState({
+  //   email: '',
+  //   password: '',
+  // })
+
+
+  // const onChange = (e) => {
+  //   setFormData((prevState) => ({
+  //     ...prevState,
+  //     [e.target.name]: e.target.value,
+  //   }))
+  // }
+
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>MenuSections</div>
+    <div className='menuSectionsContainer'>
+      <div className='sectionsList'></div>
+      <form className="defaultForm" onSubmit={onSubmit}>
+        <div className="defaultFormGroup">
+          <label htmlFor="email" className="defaultFormLabel">
+            Name
+          </label>
+          <input
+            className='defaultFormInput'
+            autoComplete="on"
+            type="name"
+            name='name'
+            // value={email}
+            placeholder='Enter your email'
+          // onChange={onChange}
+          />
+        </div>
+        <div className="defaultFormGroup">
+          <button
+            className='defaultFormButton'
+            type='submit' >
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+
   )
 }
 
