551332aec9fda751468854c8132f41d512d6b9af
Artem <silkwayway@gmail.com>
2023-10-27 15:20:49 -0400

    basic form

--- frontend/src/pages/AdminPanel/Section/Section.jsx
+++ frontend/src/pages/AdminPanel/Section/Section.jsx
@@ -1,4 +1,4 @@
-// import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams, useNavigate } from 'react-router-dom'
 // import { toast } from 'react-toastify'
@@ -12,6 +12,26 @@ function Section() {
     (state) => state.menuSections
   )
   const section = sections.find(section => section._id === sectionId);
+  // console.log(section)
+
+  const [formData, setFormData] = useState({
+    name: section.name,
+    description: section.description,
+    activeInMenu: section.activeInMenu,
+  })
+
+  const { name, description } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
 
   return (
     <div className='sectionEditContainer'>
@@ -25,7 +45,48 @@ function Section() {
         <h2><i>Section "{section?.name}"</i></h2>
       </div>
       <div className='sectionEditMainParametrs'>
-
+        <form
+          className="defaultForm"
+          onSubmit={onSubmit}>
+          <div className="defaultFormGroup">
+            <label htmlFor="name" className="defaultFormLabel">
+              Name
+            </label>
+            <input
+              className='defaultFormInput'
+              autoComplete="on"
+              type="text"
+              name='name'
+              id='name'
+              value={name}
+              placeholder='Enter name for section'
+              onChange={onChange}
+            />
+          </div>
+          <div className="defaultFormGroup">
+            <label htmlFor="description" className="defaultFormLabel">
+              Description <i>(optional)</i>
+            </label>
+            <textarea
+              className='defaultFormTextArea'
+              autoComplete="on"
+              type="textarea"
+              id='description'
+              value={description}
+              name='description'
+              placeholder='Description of Menu Section'
+              rows="3"
+              onChange={onChange}
+            />
+          </div>
+          <div className="defaultFormGroup">
+            <button
+              className='defaultFormButton'
+              type='submit' >
+              Save
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   )
