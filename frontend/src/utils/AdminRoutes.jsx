30fe9efd49f64e9c60bd9576f5cff500418036d6
Artem <silkwayway@gmail.com>
2023-10-27 14:18:48 -0400

    fix for admin middleware

--- frontend/src/utils/AdminRoutes.jsx
+++ frontend/src/utils/AdminRoutes.jsx
@@ -1,10 +1,12 @@
 import { Outlet, Navigate } from "react-router-dom"
 import { useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
 
 function AdminRoutes() {
   const user = useSelector((state) => state.auth.user);
 
-  if (!user.admin) {
+  if (!user?.admin) {
+    toast.error('This page is allowed only for admins')
     return <Navigate to="/" />
   }
   return <Outlet />
