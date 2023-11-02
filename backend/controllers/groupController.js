f34983be2e47b4373a8d9db5de3fdbbaca09385a
Artem <silkwayway@gmail.com>
2023-11-02 15:14:00 -0400

    editgroup controller

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -41,7 +41,57 @@ const getGroups = asyncHandler(async (req, res) => { })
 //* desc EDIT Group
 //* route PUT /api/menu_sections/:id/groups:groupId
 //* access Private
-const editGroup = asyncHandler(async (req, res) => { })
+const editGroup = asyncHandler(async (req, res) => {
+  ///CONSTS
+  const sectionId = req.params.id
+  // console.log('sectionId - ' + sectionId)
+  if (!sectionId) {
+    res.status(404)
+    throw new Error('There is no ID of section')
+  }
+  const groupId = req.params.groupId
+  // console.log('groupId - ' + groupId)
+  if (!groupId) {
+    res.status(404)
+    throw new Error('There is no ID of group')
+  }
+  const groupNewName = req.body.name
+  // console.log('groupNewName - ' + groupNewName)
+  if (!groupNewName) {
+    res.status(404)
+    throw new Error('There is no new name for group')
+  }
+  ///ACTIONS
+  try {
+    //Get menu section
+    const menuSection = await MenuSection.findById(req.params.id)
+    if (!menuSection) {
+      res.status(400)
+      throw new Error('Menu section is not found')
+    }
+    //Check existing of newName
+    const checkGroup = menuSection.extraIngredientTypes?.find((group) => group.name === groupNewName);
+    if (checkGroup) {
+      res.status(400)
+      throw new Error('Group with this name is already exist')
+    }
+    menuSection.extraIngredientTypes = menuSection.extraIngredientTypes?.map((group) => {
+      if (group._id.toString() === groupId) {
+        return { ...group, name: groupNewName };
+      }
+      return group;
+    })
+    await menuSection.save()
+    // console.log(menuSection)
+    const updatedCroup = menuSection.extraIngredientTypes?.find((group) => group._id.toString() === groupId);
+    // console.log(updatedCroup)
+
+    res.status(200).json(updatedCroup)
+  } catch (error) {
+    res.status(404)
+    throw new Error(error)
+  }
+})
 
 //* desc GET Group
 //* route GET /api/menu_sections/:id/groups:groupId
