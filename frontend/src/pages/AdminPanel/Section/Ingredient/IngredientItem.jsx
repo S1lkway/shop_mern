48e82b5de77eae36cd2e2e3900a036b80799d1ce
Artem <silkwayway@gmail.com>
2023-11-07 15:49:00 -0500

    css for ingredients list

--- frontend/src/pages/AdminPanel/Section/Ingredient/IngredientItem.jsx
+++ frontend/src/pages/AdminPanel/Section/Ingredient/IngredientItem.jsx
@@ -2,16 +2,56 @@ import React from 'react'
 
 function IngredientItem(props) {
   // const section = props.section
-  const group = props.group
+  // const group = props.group
+  const ingredient = props.ingredient
   // console.log(group)
   // const ingredients = group.ingredients
 
   return (
-    <div className='IngredientItem'>
-      <div className='ingredientsListHeader'>
-        <h4>Additional ingredients for "{group.name}"</h4>
-      </div>
-      <div className="listOfIngredients">listOfIngredients</div>
+    <div className='ingredientItem'>
+      {ingredient.name}
+      {/* <form
+        className="defaultForm"
+        onSubmit={onSubmit}>
+        <div className="defaultFormGroup">
+          <label htmlFor="name" className="defaultFormLabel">
+            Name
+          </label>
+          <input
+            className='defaultFormInput'
+            autoComplete="on"
+            type="text"
+            name='name'
+            id='name'
+            value={name}
+            placeholder='Enter name for section'
+            onChange={onChange}
+          />
+        </div>
+        <div className="defaultFormGroup">
+          <label htmlFor="description" className="defaultFormLabel">
+            Description <i>(optional)</i>
+          </label>
+          <textarea
+            className='defaultFormTextArea'
+            autoComplete="on"
+            type="textarea"
+            id='description'
+            value={description}
+            name='description'
+            placeholder='Description of Menu Section'
+            rows="3"
+            onChange={onChange}
+          />
+        </div>
+        <div className="defaultFormGroup">
+          <button
+            className='defaultFormButton'
+            type='submit' >
+            Save
+          </button>
+        </div>
+      </form> */}
     </div>
   )
 }
--- frontend/src/pages/AdminPanel/Section/Ingredient/IngredientList.jsx
+++ frontend/src/pages/AdminPanel/Section/Ingredient/IngredientList.jsx
@@ -11,7 +11,14 @@ function IngredientList(props) {
     <div className='groupIngridientsList'>
       {(group?.ingredients.length > 0) ?
         (
-          <IngredientItem section={section} group={group} />
+          <>
+            <div className='ingredientsListHeader'>
+              <h4>Additional ingredients for "{group.name}":</h4>
+            </div>
+            {group.ingredients.map((ingredient) => (
+              <IngredientItem key={ingredient._id} section={section} group={group} ingredient={ingredient} />
+            ))}
+          </>
         ) :
         (
           <h3>Group "{group.name}" doesn't have additional ingredients</h3>
