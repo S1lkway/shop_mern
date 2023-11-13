0548c7a5e2a43a451bef97559f702a03d55de8bc
Artem <silkwayway@gmail.com>
2023-11-13 16:50:29 -0500

    ingredients list basic

--- frontend/src/pages/AdminPanel/Section/Ingredient/IngredientItem.jsx
+++ frontend/src/pages/AdminPanel/Section/Ingredient/IngredientItem.jsx
@@ -1,13 +1,37 @@
+
+//-MUI icons
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+
 function IngredientItem(props) {
   // const section = props.section
   // const group = props.group
   const ingredient = props.ingredient
-  // console.log(group)
+  // console.log(ingredient)
   // const ingredients = group.ingredients
 
   return (
     <div className='ingredientItem'>
-      {ingredient.name}
+      <div className='ingredientHeader'>
+        <div className="ingredientHeaderLeft">
+          <button
+            className='defaultFormButton ingredientButton'
+            // onClick={() => setPickedGroup(groupIndex)}
+            title="Open Ingredient's properties">
+            {ingredient.name}
+          </button>
+        </div>
+        <div className="ingredientHeaderRight">
+          <button
+            // onClick={() => { deleteGroup(group._id) }}
+            title='Delete ingredient'
+            className='defaultFormButton deleteGroupButton'>
+            <DeleteOutlinedIcon />
+          </button>
+        </div>
+      </div>
+      <div className='ingredientProperties'>
+
+      </div>
     </div>
   )
 }
