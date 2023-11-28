f1e0712696ba555df532c19195a397512f29c524
Artem <silkwayway@gmail.com>
2023-11-28 10:52:02 -0500

    picked link for admin panel

--- frontend/src/pages/AdminPanel/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel/AdminPanel.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react"
 //-Components
 import SectionList from "./SectionList"
 
 
 function AdminPanel() {
+  const [pickedLink, setPickedLink] = useState('Section')
 
   return (
     <div className="adminPanelContainer">
-      <div className="adminPanelHeader"></div>
+      <div className="adminPanelHeader">
+        <div
+          onClick={() => setPickedLink('Section')}
+          className={pickedLink === 'Section' ? ('adminPanelHeaderLink pickedLink') : ('adminPanelHeaderLink')}
+          title='Sections'>
+          <h2>Sections</h2>
+        </div>
+        <div
+          onClick={() => setPickedLink('Dishes')}
+          className={pickedLink === 'Dishes' ? ('adminPanelHeaderLink pickedLink') : ('adminPanelHeaderLink')}
+          title='Dishes'>
+          <h2>Dishes</h2>
+        </div>
+      </div>
       <SectionList />
     </div>
   )
