9e7b0ad52715a4ae8170945c6bb9d5a864c7cb6f
Artem <silkwayway@gmail.com>
2024-03-27 08:11:17 -0400

    updating

--- frontend/src/pages/AdminPanel/Dish/CreateDish.jsx
+++ frontend/src/pages/AdminPanel/Dish/CreateDish.jsx
@@ -61,23 +61,8 @@ function CreateDish() {
   }
 
 
-
   return (
-
-
-
-
-
-
-
-
-
-
-
     <div className='createDishContainer'>
-
-
-
       <div className='createDishTitle'>
         <h2>Main parameters</h2>
       </div>
