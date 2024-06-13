40babe6d4cabbb0cee1a530aa8649c2aa497b38e
Artem <silkwayway@gmail.com>
2024-06-13 19:50:06 -0400

    updating

--- frontend/src/pages/AdminPanel/Dish/CreateDish.jsx
+++ frontend/src/pages/AdminPanel/Dish/CreateDish.jsx
@@ -58,11 +58,18 @@ function CreateDish() {
       dishData.append('images', file[0]);
       console.log([...dishData.entries()])
     }
+
+
   }
 
   return (
 
+
+
     <div className='createDishContainer'>
+
+
+
 
       <div className='createDishTitle'>
 
