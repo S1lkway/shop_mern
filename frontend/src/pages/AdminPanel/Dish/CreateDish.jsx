0314119096dd5468893209637e617a0ea66de8ae
Artem <silkwayway@gmail.com>
2024-08-20 19:49:59 -0400

    updating

--- frontend/src/pages/AdminPanel/Dish/CreateDish.jsx
+++ frontend/src/pages/AdminPanel/Dish/CreateDish.jsx
@@ -65,14 +65,10 @@ function CreateDish() {
 
     <div className='createDishContainer'>
       <div className='createDishTitle'>
-
-
         <h2>Main parameters</h2>
 
 
       </div>
-
-
       <form
         onSubmit={createNewDish}
         className="defaultForm">
